e7f05924dfd5180057c1326d2679cf5ef4ab2981
Yale Cason <YCason@tql.com>
2016-09-20 17:16:50 -0400

    init

---
+++ MockLibraryRunner/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RealLibrary;
+
+namespace MockLibraryRunner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cls = new Class1();
+
+            Console.WriteLine(cls.ToString());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MockLibraryRunner/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MockLibraryTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("TQL")]
+[assembly: AssemblyProduct("MockLibraryTest")]
+[assembly: AssemblyCopyright("Copyright © TQL 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8084e141-cd14-43d7-9044-1f1feccea100")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
